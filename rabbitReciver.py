6d2dc2b402eef78b220451cc0a4fdce01d448769
Michal Zawadzki <michal.zawadzki66669@gmail.com>
2022-01-22 11:37:43 +0100

    dodanie blodku try catch

--- rabbitReciver.py
+++ rabbitReciver.py
@@ -8,18 +8,23 @@ def main():
     channel = connection.channel()
     channel.queue_declare(queue=nameQueue)
     def callback(ch, method, properties, body):
-        jsonData=json.loads(body)
-        receiverEmail=jsonData['emailAddress']
-        typeMessage=jsonData['notificationType']
-        allowedTypesMessage=["PARCEL_REGISTERED_SENDER","PARCEL_REGISTERED_RECEIVER","COURIER_WILL_ARRIVE","PARCEL_DELIVERED","PARCEL_WILL_ARRIVE_TODAY"]
-        if (typeMessage in allowedTypesMessage):
-            mailHTML=MailHTML(typeMessage)
-            html = mailHTML.generateHTML(jsonData)
-            senderMail=SenderMail(receiverEmail,html)
-            senderMail.sendMail()
-            print("mail wyslany do ",receiverEmail)
-        else:
-            print("Not correct type of message",receiverEmail)
+        try:
+            jsonData=json.loads(body.decode("utf-8"))
+            receiverEmail=jsonData['emailAddress']
+            typeMessage=jsonData['notificationType']
+            allowedTypesMessage=["PARCEL_REGISTERED_SENDER","PARCEL_REGISTERED_RECEIVER","COURIER_WILL_ARRIVE","PARCEL_DELIVERED","PARCEL_WILL_ARRIVE_TODAY"]
+            if (typeMessage in allowedTypesMessage):
+                mailHTML=MailHTML(typeMessage)
+                html = mailHTML.generateHTML(jsonData)
+                senderMail=SenderMail(receiverEmail,html)
+                senderMail.sendMail()
+                print("mail wyslany do ",receiverEmail)
+            else:
+                print("Not correct type of message",receiverEmail)
+        except:
+            print("Mail not send")
+
+
 
     channel.basic_consume(queue=nameQueue, on_message_callback=callback, auto_ack=True)
     print(' [*] Waiting for messages. To exit press CTRL+C')
